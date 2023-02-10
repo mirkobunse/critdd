cbe5f0d48d6e710637eae6f838f218b6503a292b
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2023-02-10 04:35:19 +0100

    First steps towards tikz generation: tikzpicture and axis

--- critdd/tikz.py
+++ critdd/tikz.py
@@ -1,10 +1,10 @@
 """A module for the Tikz export."""
 
-import os
+import os, re
 
 def to_str(average_ranks, cliques):
     """Return a string with Tikz code."""
-    return "TODO\n...\n" # TODO
+    return _tikzpicture(_axis("TODO", ["title={foo}", "axis x line=center"])) # TODO
 
 def to_file(path, average_ranks, cliques):
     """Store the Tikz code in a file."""
@@ -21,3 +21,16 @@ def _to_tikz_file(path, average_ranks, cliques):
     """Store the Tikz code in a .tex or .tikz file."""
     with open(path, "w") as f:
         f.write(to_str(average_ranks, cliques))
+
+def _tikzpicture(content):
+    return "\n".join(["\\begin{tikzpicture}[]", content, "\\end{tikzpicture}\n"])
+def _axis(content, options):
+    return "\n".join([
+        "\\begin{axis}[",
+        _indent(",\n".join(options)) + ",",
+        "]\n",
+        content,
+        "\n\\end{axis}"
+    ])
+def _indent(content):
+    return re.sub("^", "  ", content, flags=re.MULTILINE)
